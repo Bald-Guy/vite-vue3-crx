994f451d5204b251a08569153d97b30c1978ac33
Bald-Guy <bald_guy@163.com>
2023-10-15 16:25:58 +0800

    update

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 # vite-vue3-crx
+## 克隆到本地
+```shell
+npx degit Bald-Guy/vite-vue3-crx my-webext
+cd my-webext
+pnpm i
+```
+
+## 开发
+```shell
+pnpm dev
+```
+```shell
+pnpm start:chromium
+```
 
 ## 打包
+```shell
+pnpm build
+```
+
 crx 工具可能已经有一段时间没有更新了。不过，打包为 .crx 文件的需求仍然存在，尤其是为了在 Chrome 或基于 Chromium 的浏览器上分发扩展。
 
 谷歌官方推荐使用 Chrome 浏览器本身来打包 CRX 文件。以下是步骤：
@@ -12,3 +30,7 @@ crx 工具可能已经有一段时间没有更新了。不过，打包为 .crx �
 加载完成后，你的扩展程序会显示在列表中。找到它，并点击“打包扩展程序”按钮。
 选择根目录和私钥文件（如果是首次打包，可以不选择私钥文件，Chrome 会为你生成一个新的私钥文件）。点击打包后，Chrome 会为你生成 .crx 文件和 .pem 私钥文件。
 需要注意的是，这种方式打包的 .crx 文件主要用于非 Chrome Web Store 的分发。如果你打算在 Chrome Web Store 上发布扩展程序，你只需要上传已解压的扩展程序目录，Google 会在发布时自动为你打包成 .crx 格式。
+
+
+## TODO
+- [ ] 实现响应式 localStorage
--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -1,42 +1,23 @@
 <script setup lang="ts">
 import Logo from '~/components/Logo.vue'
-import { storageDemo } from '~/logic/storage'
 
 function openOptionsPage() {
   chrome.runtime.openOptionsPage()
 }
-let count = 0
-function changeStorageDemo() {
-  storageDemo.value = `changed Value${count.toString()}`
-  count++
-}
-
-function removeStorageDemo() {
-  storageDemo.value = null
-}
 </script>
 
 <template>
-  <div>
+  <div class="flex flex-col items-center">
     Popup Page
+    <Logo class="mt-4" />
+    <button class="mt-4 p-2 text-xs text-white bg-blue-300 rounded" @click="openOptionsPage">
+      Open Options Page
+    </button>
   </div>
-  <Logo />
-  <button class="text-xs bg-blue-300 rounded" @click="openOptionsPage">
-    Open Options Page
-  </button>
-  <div class="mt-2">
-    <span class="opacity-50">Storage:</span> {{ storageDemo }}
-  </div>
-  <button @click="changeStorageDemo">
-    改变缓存数据
-  </button>
-  <button @click="removeStorageDemo">
-    删除缓存数据
-  </button>
 </template>
 
 <style>
-body {
-  @apply w-96 h-96 bg-gray-100;
+#app {
+  @apply w-64 h-80;
 }
 </style>
